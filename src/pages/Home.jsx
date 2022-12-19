85bac71dea5435dfd31d06b788512cabe09b07f2
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-19 22:00:53 +0100

    Implemented search feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Header from '../components/Header'
 import MovieDetails from '../components/MovieDetails'
 
 export default function Home() {
-    const [movie, setMovie] = useState({})
+    const [movie, setMovie] = useState(null)
     const [search, setSearch] = useState('')
     const [searchResult, setSearchResult] = useState([])
 
