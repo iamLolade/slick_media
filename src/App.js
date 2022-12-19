22a109475964bce735ea510036568f578a2b76c9
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-19 18:05:33 +0100

    Created Home page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { Box } from '@chakra-ui/react'
 
 // components & pages
 import Navbar from './components/Navbar';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Box h='100vh'>
       <BrowserRouter>
         <Navbar />
+        <Box>
+          <Routes>
+            <Route 
+              path='/'
+              element={<Home />}
+            />
+          </Routes>
+        </Box>
       </BrowserRouter> 
     </Box>
   );
